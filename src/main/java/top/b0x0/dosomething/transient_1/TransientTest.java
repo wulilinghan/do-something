d142ce5981fbffe5894383d134c078d34f4d880f
tlh <im.linghan@qq.com>
2021-08-20 13:24:48 +0800

    feat: 使用 transient 关键字不序列化某个变量

--- src/main/java/top/b0x0/dosomething/transient_1/TransientTest.java
+++ src/main/java/top/b0x0/dosomething/transient_1/TransientTest.java
@@ -1,11 +1,12 @@
-package top.b0x0.dosomething;
+package top.b0x0.dosomething.transient_1;
 
 import java.io.*;
 
 /**
- * @author Alexia
- * @description 使用transient关键字不序列化某个变量
+ * 使用 transient 关键字不序列化某个变量
  * 注意读取的时候，读取数据的顺序一定要和存放数据的顺序保持一致
+ *
+ * @author Alexia
  * @date 2013-10-15
  */
 public class TransientTest {
@@ -13,32 +14,36 @@ public class TransientTest {
     public static void main(String[] args) {
 
         User user = new User();
-        user.setUsername("Alexia");
+        user.setUsername("ManJiis");
         user.setPasswd("123456");
 
         System.out.println("read before Serializable: ");
         System.out.println("username: " + user.getUsername());
         System.err.println("password: " + user.getPasswd());
 
+        // 1. 将user数据写入文件
         try {
-            ObjectOutputStream os = new ObjectOutputStream(
-                    new FileOutputStream("d:/user.txt"));
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("d:/user.txt"));
+
             // 将User对象写进文件
-            os.writeObject(user);
-            os.flush();
-            os.close();
+            oos.writeObject(user);
+
+            oos.flush();
+            oos.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // 2。 将user数据从文件中读取出来
         try {
             // 在反序列化之前改变username的值
-            User.username = "jmwang";
+            User.username = "ManJiis222";
 
-            ObjectInputStream is = new ObjectInputStream(new FileInputStream(
-                    "d:/user.txt"));
+            ObjectInputStream oip = new ObjectInputStream(new FileInputStream("d:/user.txt"));
+
             // 从流中读取User的数据
-            user = (User) is.readObject();
-            is.close();
+            user = (User) oip.readObject();
+            oip.close();
 
             System.out.println("\nread after Serializable: ");
             System.out.println("username: " + user.getUsername());
@@ -61,7 +66,7 @@ class User implements Serializable {
     }
 
     public void setUsername(String username) {
-        this.username = username;
+        User.username = username;
     }
 
     public String getPasswd() {
